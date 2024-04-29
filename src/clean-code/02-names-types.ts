d893f0e2c3c645b34f40738e3da2fa0b067e2538
JKmoises <moi.prado@duocuc.cl>
2024-04-28 21:54:53 -0400

    Refactor variable and function names in clean-code files

--- src/clean-code/02-names-types.ts
+++ src/clean-code/02-names-types.ts
@@ -1,53 +1,53 @@
 (() => {
   // arreglo de temperaturas celsius
-  const arrayOfNums = [33.6, 12.34];
+  const temperaturesCelsius = [33.6, 12.34];
 
   // Dirección ip del servidor
-  const ip = "123.123.123.123";
+  const serverIp = "123.123.123.123";
 
   // Listado de usuarios
-  const people = [
+  const users = [
     { id: 1, email: "fernando@google.com" },
     { id: 2, email: "juan@google.com" },
     { id: 3, email: "melissa@google.com" },
   ];
 
   // Listado de emails de los usuarios
-  const emails = people.map((u) => u.email);
+  const userEmails = users.map((user) => user.email);
 
   // Variables booleanas de un video juego
-  const jump = false;
-  const run = true;
-  const noTieneItems = true;
-  const loading = false;
+  const canJump = false;
+  const canRun = true;
+  const hasItems = true;
+  const isLoading = false;
 
   // Otros ejercicios
   // tiempo inicial
-  const start = new Date().getTime();
+  const initialTime = new Date().getTime();
   //....
   // 3 doritos después
   //...
   // Tiempo al final
-  const end = new Date().getTime() - start;
+  const finalTime = new Date().getTime() - initialTime;
 
   // Funciones
   // Obtiene los libros
-  function book() {
+  function getBooks() {
     throw new Error("Function not implemented.");
   }
 
   // obtiene libros desde un URL
-  function BooksUrl(u: string) {
+  function getBooksByUrl(url: string) {
     throw new Error("Function not implemented.");
   }
 
   // obtiene el área de un cuadrado basado en sus lados
-  function areaCuadrado(s: number) {
+  function getSquareArea(side: number) {
     throw new Error("Function not implemented.");
   }
 
   // imprime el trabajo
-  function printJobIfJobIsActive() {
+  function printJob() {
     throw new Error("Function not implemented.");
   }
 })();
--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,31 +1,34 @@
 (() => {
   // función para obtener información de una película por Id
-  function getAllMovies(movieId: string) {
-    console.log({ movieId });
+  function getMovieById(id: string) {
+    console.log({ id });
   }
 
   // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getAllMovieActors(id: string) {
+  function getMovieCastById(id: string) {
     console.log({ id });
   }
 
   // funcion para obtener el bio del actor por el id
-  function getUsuario(ActorId: string) {
-    console.log({ ActorId });
+  function getActorBioById(id: string) {
+    console.log({ id });
+  }
+
+
+  interface Movie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
   }
 
   // Crear una película
-  function movie(
-    title: string,
-    description: string,
-    rating: number,
-    cast: string[]
-  ) {
+  function movie({title,description,rating,cast}: Movie) {
     console.log({ title, description, rating, cast });
   }
 
   // Crea un nuevo actor
-  function createActorIfActorNotExists(
+  function createActor(
     fullName: string,
     birthdate: Date
   ): boolean {
@@ -38,31 +41,17 @@
     return true;
   }
 
+  // Tarea extra - usar solo if sin tanta anidacion
   const getPayAmount = ({
     isDead = false,
     isSeparated = true,
     isRetired = false,
   }) => {
-    let result;
+    if (isDead) return 1500;
 
-    if (isDead) {
-      result = 1500;
-    } else {
-
-      if (isSeparated) {
-        result = 2500;
-        
-      } else {
-
-        if (isRetired) {
-          result = 3000;
-        } else {
-          result = 4000;
-        }
-
-      }
-    }
-
-    return result;
+    if (isSeparated) return 2500;
+    
+    return isRetired ? 3000 : 4000;
+     
   };
 })();
